f75aa35763b917b18687b5de111185999066bbc8
ApocalypticSharks <pastuhovv@gmail.com>
2023-06-20 10:32:32 +1000

    grimoire state

--- Obscurio/Assets/BrokenVector/LowPolyDungeon/Scripts/GameStateSystem.cs
+++ Obscurio/Assets/BrokenVector/LowPolyDungeon/Scripts/GameStateSystem.cs
@@ -7,12 +7,20 @@ using UnityEngine;
 public class GameStateSystem : NetworkBehaviour
 {
     public NetworkVariable<FixedString64Bytes> gameState = new NetworkVariable<FixedString64Bytes>("readyCheck", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    [SerializeField]private RoomSpawner roomSpawner;
 
     [ServerRpc]
     public void ChangeGameStateServerRpc(string state)
     {
         gameState.Value = state;
-        CallOnChangeStatePlayerActionClientRpc(gameState.Value);
+
+        switch (gameState.Value.ToString())
+        {
+            case "grimoire":
+                CallOnChangeStatePlayerActionClientRpc(gameState.Value);
+                roomSpawner.SpawnFirstRoomServerRpc();
+                break;
+        }
     }
     [ClientRpc]
     public void CallOnChangeStatePlayerActionClientRpc(FixedString64Bytes state)
--- Obscurio/Assets/BrokenVector/LowPolyDungeon/Scripts/RoomSpawner.cs
+++ Obscurio/Assets/BrokenVector/LowPolyDungeon/Scripts/RoomSpawner.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Netcode;
 using UnityEngine;
 
-public class RoomSpawner : MonoBehaviour
+public class RoomSpawner : NetworkBehaviour
 {
     [SerializeField] private List<Object> roomsList = new List<Object>();
     [SerializeField] private List<Transform> roomSpawnPointsList = new List<Transform>();
@@ -15,10 +16,31 @@ public class RoomSpawner : MonoBehaviour
         {
             roomsPool.Add(new Rooms(roomsList[i]));
         }
-        for (int i = 0; i < roomSpawnPointsList.Count; i++)
+    }
+
+    [ServerRpc]
+    public void SpawnFirstRoomServerRpc()
+    {
+        SpawnRoom();
+    }
+
+    [ServerRpc]
+    public void SpawnAllRoomsServerRpc()
+    {
+        for (int i = 0; i < roomSpawnPointsList.Count - 1; i++)
+        {
+            SpawnRoom();
+        }
+
+    }
+
+    private void SpawnRoom()
+    {
+        List<Transform> freeRoomLocations = roomSpawnPointsList.Where(roomLocation => roomLocation.childCount == 0).ToList();
+        if (freeRoomLocations.Count > 0)
         {
             List<Rooms> freeRooms = roomsPool.Where(room => !room.roomEntity.activeSelf).ToList();
-            freeRooms[Random.Range(0, freeRooms.Count)].ActivateDeactivte(roomSpawnPointsList[i]);
+            freeRooms[Random.Range(0, freeRooms.Count)].ActivateDeactivte(roomSpawnPointsList[Random.Range(0, freeRoomLocations.Count)]);
         }
     }
 }
