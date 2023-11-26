50c7c49fadf0d850308ee83b20e71ed30bafd99a
ApocalypticSharks <pastuhovv@gmail.com>
2023-11-26 21:37:24 +1000

    Fart through realms new project

---
+++ Fart through realms/Assets/Scripts/Input/InputSystem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSystem : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private PlayerInputs playerInputs;
+    [SerializeField] private Transform head, bottom;
+    [SerializeField] private float rotationSpeed, maxChargePower;
+    [SerializeField]private float chargePower = 1, chargingSpeed;
+    private bool isCharging;
+
+    private void Awake()
+    {
+        playerInputs = new PlayerInputs();
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerInputs.Player.Enable();
+        playerInputs.Player.Jump.started += JumpCharge;
+        playerInputs.Player.Jump.canceled += JumpRelease;
+    }
+
+    private void FixedUpdate()
+    {
+        float rotationDirection = playerInputs.Player.Rotate.ReadValue<float>();
+        transform.Rotate(new Vector3(0,0,-5) * rotationDirection);
+        if (isCharging && chargePower < maxChargePower)
+            chargePower += chargingSpeed * Time.deltaTime;
+    }
+
+    public void JumpCharge(InputAction.CallbackContext context)
+    {
+        isCharging = true;
+    }
+
+    public void JumpRelease(InputAction.CallbackContext context) {
+        isCharging = false;
+        int layerMask = 1 << 3;
+        chargePower = chargePower < maxChargePower ? chargePower : maxChargePower;
+        if (Physics2D.Raycast(transform.position, -transform.up, 2f, layerMask))
+        {
+            rigidBody.AddForce(transform.up.normalized * chargePower, ForceMode2D.Impulse);
+        }         
+        chargePower = 1;
+    }
+}
